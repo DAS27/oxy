a7531e597b5f8b2569c1976fab0d9b15794e70bf
Damir Sarkulin <sarkulindamir@gmail.com>
2021-11-17 02:04:28 +0500

    added get all articles

--- Modules/Blog/Entities/Article.php
+++ Modules/Blog/Entities/Article.php
@@ -4,15 +4,21 @@ namespace Modules\Blog\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\DB;
 
 class Article extends Model
 {
     use HasFactory;
 
     protected $fillable = [];
-    
+
     protected static function newFactory()
     {
         return \Modules\Blog\Database\factories\ArticleFactory::new();
+    }
+
+    public static function getArticles()
+    {
+        return DB::table('articles')->orderBy('created_at')->paginate(25);
     }
 }
