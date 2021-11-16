b227de5613eee13a032f733d6d30d282d9c8ba2a
Damir Sarkulin <sarkulindamir@gmail.com>
2021-11-17 01:23:32 +0500

    hotfix! edited constrained table name

--- Modules/Blog/Database/Migrations/2021_11_16_192243_create_articles_table.php
+++ Modules/Blog/Database/Migrations/2021_11_16_192243_create_articles_table.php
@@ -15,7 +15,7 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('user');
+            $table->foreignId('user_id')->constrained('users');
             $table->string('title');
             $table->longText('content');
             $table->timestamps();
